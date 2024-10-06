49b3f15c8f7c9a2d85d4aff3c306626e2b7511b4
Joy Banerjee <joybanerjee08@hotmail.com>
2024-10-06 23:13:29 +0530

    Added another tip

--- webapp.py
+++ webapp.py
@@ -75,6 +75,7 @@ def main():
     st.markdown("3.Numerical columns (Discreet or Continuous) must contain only numbers, not spaces or text")
     st.markdown("4.Output depends on how well defined the problem statement, data description and column descriptions are")
     st.markdown("5.Problem statement and Data description cannot be greater than 1500 words and column description cannot be greater than 100 words")
+    st.markdown("6.Try not to use any special characters in your header of the csv file, only alphabets,digits,period,hyphen and underscore")
     st.divider()
 
     uploaded_file1 = st.file_uploader("Choose a CSV file with headers", type=["csv"])
